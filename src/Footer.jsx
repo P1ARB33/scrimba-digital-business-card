9075cfaaeff641a5ba5a4f2bcdd8857468677785
P1ARB33 <parevalo@academic.rrc.ca>
2026-06-07 01:14:57 -0500

    fix: image linking and importing so that it deploys better on github pages

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,14 @@
+import InstagramLogo from "../images/instagram-icon-footer.png"
+import GitHubLogo from "../images/github-icon-footer.png"
+
 export default function Footer() {
     return (
         <footer className="socials-container">
             <a href="https://www.instagram.com/patrickrba_">
-                <img src="../images/instagram-icon-footer.png" alt="Instagram link"/>
+                <img src={InstagramLogo} alt="Instagram link"/>
             </a>
             <a href="https://github.com/P1ARB33">
-                <img src="../images/github-icon-footer.png" alt="GitHub Link"/>
+                <img src={GitHubLogo} alt="GitHub Link"/>
             </a>
         </footer>
     )
