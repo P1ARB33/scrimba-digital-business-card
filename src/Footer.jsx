d26325a6c2e414423b15ec87049a3538ca942d8b
P1ARB33 <parevalo@academic.rrc.ca>
2026-06-07 00:43:26 -0500

    feat: socials footer added.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
     return (
-        <footer>
-            {/* Footer with socials go here */}
+        <footer className="socials-container">
+            <a href="https://www.instagram.com/patrickrba_">
+                <img src="../images/instagram-icon-footer.png" alt="Instagram link"/>
+            </a>
+            <a href="https://github.com/P1ARB33">
+                <img src="../images/github-icon-footer.png" alt="GitHub Link"/>
+            </a>
         </footer>
     )
 }
