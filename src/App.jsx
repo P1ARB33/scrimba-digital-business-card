5895fe3f0284b83fb9de35a2626b81b09f26991b
P1ARB33 <parevalo@academic.rrc.ca>
2026-06-07 00:09:39 -0500

    feat: header CSS styling.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Content from "./Content"
 
 export default function App() {
     return (
-        <main>
+        <main className="card">
             <Header />
             <Content />
             <Footer />
