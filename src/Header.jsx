fb8c067d013bb347047684ba4d0c61869eb14157
P1ARB33 <parevalo@academic.rrc.ca>
2026-06-06 23:06:00 -0500

    feat: added html elements for header.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,15 @@
 export default function Header() {
     return (
         <header>
-            {/* Photo and Info goes here */}
+            <img src="../images/headshot-photo.jpg" alt="Patrick's headshot" />
+            <h1 className="profile-Name">Patrick Arevalo</h1>
+            <h2 className="profession-title">Software Developer</h2>
+            <a href="https://github.com/P1ARB33" className="github-button">
+                <span>GitHub</span>
+            </a>
+            <a href="www.linkedin.com/in/patrick-raymond-arevalo-b9a718170" className="linkedin-button">
+                <span>LinkedIn</span>
+            </a>
         </header>
     )
 }
